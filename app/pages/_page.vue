bf91de57231d485bdf662f1602e2358fc91aa09d
Max den Hoed <maxdenhoed@M1-Macbook-Pro.home>
2023-01-07 14:30:34 +0100

    rrrr

--- app/pages/_page.vue
+++ app/pages/_page.vue
@@ -1,14 +1,11 @@
 <template>
   <section class="page" :class="page.slug">
     <div class="text-center">
-    <img :src="'https://source.unsplash.com/random/1920x1080'" class="my-4" />
+    <img :src="'https://source.unsplash.com/random/1920x800'" class="my-4 mx-auto" />
     </div>
     <h1 class="page__title text-lg md:text-xl lg:text-4xl xl:text-6xl text-center py-8 md:py-16">
       {{ page.title }}
     </h1>
-    <div class="text-center">
-    <img :src="'https://source.unsplash.com/random/1920x1080'" class="my-4" />
-    </div>
 
     <div v-html="$md.render(page.content)" class="page__content markdown pt-4 md:pt-6 md:pb-24" />
 
